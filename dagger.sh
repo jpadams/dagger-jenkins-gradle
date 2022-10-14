03abce46fe022355f866bef465bd2e40485793a4
Jeremy Adams <jeremy@dagger.io>
2022-10-14 13:54:37 -0700

    Update cache API
    Signed-off-by: Jeremy Adams <jeremy@dagger.io>

--- dagger.sh
+++ dagger.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-RUN="dagger do -f ./queries.graphql"
-CACHE=$($RUN Cache | jq -r '.cacheFromTokens.id')
+RUN="/usr/local/bin/dagger do -f ./queries.graphql"
+CACHE=$($RUN Cache | jq -r '.cacheVolume.id')
 WORKFS=$($RUN Workdir | jq -r '.host.workdir.read.id')
 
 GRADLE=$($RUN --set "workfs=$WORKFS" --set "cachevol=$CACHE" Gradle | jq -r '.. | .contents? | select(. != null)')
