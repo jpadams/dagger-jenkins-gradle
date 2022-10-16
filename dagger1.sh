5019bfe770c538429219fb4a29758329f71edf90
Jeremy Adams <jeremy@dagger.io>
2022-10-16 10:08:59 -0700

    update
    Signed-off-by: Jeremy Adams <jeremy@dagger.io>

--- dagger1.sh
+++ dagger1.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+#set -x
+
 RUN="/usr/local/bin/dagger do -f ./queries.graphql"
+
 CACHE=$($RUN Cache | jq -r '.cacheVolume.id')
 
 WORKFS=$($RUN Workdir | jq -r '.host.workdir.read.id')
-GRADLE=$($RUN --set "workfs=$WORKFS" --set "cachevol=$CACHE" --set "cmd=run" Gradle | jq -r '.. | .contents? | select(. != null)')
-echo $GRADLE 
+
+GRADLE=$($RUN --set "workfs=$WORKFS" --set "cachevol=$CACHE" --set "cmd=run" Gradle)
+
+echo $GRADLE | jq -r '.. | .contents? | select(. != null)'
--- dagger2.sh
+++ dagger2.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+#set -x
+
 RUN="/usr/local/bin/dagger do -f ./queries.graphql"
+
 CACHE=$($RUN Cache | jq -r '.cacheVolume.id')
 
 JUNIT=$($RUN JUnitExample | jq -r '.git.branch.tree.directory.id')
-GRADLE=$($RUN --set "workfs=$JUNIT" --set "cachevol=$CACHE" --set "cmd=test" Gradle | jq -r '.. | .contents? | select(. != null)')
-echo $GRADLE 
+
+GRADLE=$($RUN --set "workfs=$JUNIT" --set "cachevol=$CACHE" --set "cmd=test" Gradle)
+
+echo $GRADLE | jq -r '.. | .contents? | select(. != null)'
